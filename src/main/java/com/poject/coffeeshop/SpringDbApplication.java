fccb7eaa42c877685117812bed5c09de62f0e139
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-05-17 11:04:46 +0500

    docker db

---
+++ src/main/java/com/poject/coffeeshop/SpringDbApplication.java
@@ -0,0 +1,31 @@
+package com.poject.coffeeshop;
+
+import com.poject.coffeeshop.entity.Product;
+import com.poject.coffeeshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringDbApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbApplication.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception{
+        Product product = new Product(null, "name");
+        repository.save(product);
+        List<Product> all = repository.findAll();
+        System.out.println(all);
+    }
+
+
+
+}
---
+++ src/main/java/com/poject/coffeeshop/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.poject.coffeeshop.repository;
+
+import com.poject.coffeeshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository  extends JpaRepository<Product, Long> {
+}
