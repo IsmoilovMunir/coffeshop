58b39b64a8d678263c62e800c3a003f33cc8bea6
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-05-21 23:00:23 +0500

    gitignor

---
+++ src/main/resources/db/changelog/v1/date.sql
@@ -0,0 +1,3 @@
+insert into product(name, description)
+values
+('latte','кофейна напиток 1')
--- src/main/resources/db/changelog/v1/scheme.sql
+++ src/main/resources/db/changelog/v1/scheme.sql
@@ -4,6 +4,3 @@ create table product
     name VARCHAR(255),
     description VARCHAR(10000)
 );
-insert into product(name, description)
-values
-('latte','кофейна напиток 1')
