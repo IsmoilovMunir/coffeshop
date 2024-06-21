81400874919d9eddb0f0725a6309116977f09017
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-06-21 21:24:44 +0500

    bootstrap and css

--- src/main/java/com/poject/coffeeshop/controllers/CustomExceptionController.java
+++ src/main/java/com/poject/coffeeshop/controllers/CustomExceptionController.java
@@ -2,25 +2,29 @@ package com.poject.coffeeshop.controllers;
 
 import com.poject.coffeeshop.exceptions.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@ControllerAdvice
 public class CustomExceptionController {
-    @ExceptionHandler(EntityNotFoundException.class)
+
+    @ExceptionHandler({EntityNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleEntityNotFoundException(EntityNotFoundException ex, Model model){
-        model.addAttribute("errorMessage", ex.getMessage());
-        return "error/not_found";
+    public ModelAndView handleEntityNotFound(EntityNotFoundException exception){
+        ModelAndView modelAndView = new ModelAndView("/error/not_found");
+        modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler({Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleException(Exception ex, Model model){
-        model.addAttribute("exceptionMsg", ex.getMessage());
-        model.addAttribute("stackTrace", ex.getStackTrace());
-        return "error/generic_error";
+    public ModelAndView handleException(Exception exception){
+        ModelAndView modelAndView = new ModelAndView("/error/generic_error");
+        modelAndView.addObject("message", exception.getMessage());
+        modelAndView.addObject("exception", exception.getStackTrace());
+        return modelAndView;
     }
 }
