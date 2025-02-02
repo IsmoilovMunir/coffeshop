d0d35d1ba60b9aedfca83ddcaa8f63b94df975ff
MunirIsmoilov <ismoilov.munir@gmail.com>
2025-02-02 23:28:06 +0300

    menu

--- src/main/java/com/poject/coffeeshop/SpringDbApplication.java
+++ src/main/java/com/poject/coffeeshop/SpringDbApplication.java
@@ -1,9 +1,5 @@
 package com.poject.coffeeshop;
 
-import com.poject.coffeeshop.entity.Product;
-import com.poject.coffeeshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +7,7 @@ import java.util.List;
 
 @SpringBootApplication
 public class SpringDbApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(SpringDbApplication.class, args);
-
-
     }
 }
